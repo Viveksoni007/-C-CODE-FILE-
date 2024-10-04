db7d7559564a8714d36155e73a4e19a914a095a3
Viveksoni007 <consolegamingplay@gmail.com>
2024-10-04 19:53:05 +0530

    Update HELLO WORLD.C

--- CHAPTER 1/New folder/HELLO WORLD.C
+++ CHAPTER 1/New folder/HELLO WORLD.C
@@ -2,7 +2,7 @@
 int main() 
 {
     //print kkarna hai heelo world progrma this void only use to print he message in on only this called void main;
-    printf("hello world ilove you i am doing coading with c language in date 14 sep 2024 saturday \n ");
+    printf("hello world i am doing coading with c language in date 14 sep 2024 saturday \n ");
     printf("vivek\n \n");
     return 0;
 }
